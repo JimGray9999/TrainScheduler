f6305578a0f7d2dbd01a6247016aea52ef46616e
Jim Gray <jigray@corp.lpl.com>
2017-07-11 14:01:51 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,43 @@
-$(document).ready(function(){
-	// Initialize Firebase
-		var config = {
-			apiKey: "AIzaSyA_btVeabqa-okXDtYE9Q6_pE0YaYCN0I8",
-			authDomain: "train-scheduler-38085.firebaseapp.com",
-			databaseURL: "https://train-scheduler-38085.firebaseio.com",
-			projectId: "train-scheduler-38085",
-			storageBucket: "train-scheduler-38085.appspot.com",
-			messagingSenderId: "914799628028"
-		};
-		firebase.initializeApp(config);
+$(document).ready(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA_btVeabqa-okXDtYE9Q6_pE0YaYCN0I8",
+        authDomain: "train-scheduler-38085.firebaseapp.com",
+        databaseURL: "https://train-scheduler-38085.firebaseio.com",
+        projectId: "train-scheduler-38085",
+        storageBucket: "train-scheduler-38085.appspot.com",
+        messagingSenderId: "914799628028"
+    };
+    firebase.initializeApp(config);
 
-	// initial variables
+    // initial variables
+    var database = firebase.database();
 
-	// inital load, listener for train additions
+    // inital load, listener for train additions
 
-		// load current trains in the firebase database
-		
-		// dynamically update HTML table, add new train to bottom
-		
-	// on click for train submit button
-	$("#new-train").on("click", function(){
-		// stop blank form submission
-		event.preventDefault();
+    // load current trains in the firebase database
 
-		// capture values from inputs
-		var trainName = $("#add-train").val().trim();
-		var trainDest = $("#add-destination").val().trim();
-		var trainFreq = $("#add-freq").val().trim();
-		var train1st = $("#add-1st-train").val().trim();
+    // dynamically update HTML table, add new train to bottom
 
-		// console log test
-		console.log(trainName);
-		console.log(trainDest);
-		console.log(trainFreq);
-		console.log(train1st);
+    // on click for train submit button
+    $("#new-train").on("click", function() {
+        // stop blank form submission
+        event.preventDefault();
 
-		// add record to firebase
+        // capture values from inputs
+        var trainName = $("#add-train").val().trim();
+        var trainDest = $("#add-destination").val().trim();
+        var trainFreq = $("#add-freq").val().trim();
+        var train1st = $("#add-1st-train").val().trim();
 
-		// clear values from form
-	});
+        // console log test
+        console.log(trainName);
+        console.log(trainDest);
+        console.log(trainFreq);
+        console.log(train1st);
+
+        // add record to firebase
+
+        // clear values from form
+    });
 });
